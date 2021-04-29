bb5d90cfda95d64f58ed9426ea0c09571ae0c102
LongNHH <fvn.longnhh@gmail.com>
2021-04-29 17:27:32 +0700

    new order

---
+++ js/jquery.scroll.js
@@ -0,0 +1,44 @@
+// anchor in page
+$(window).bind('load',function(){
+	"use strict";
+	$(function(){
+		$('a[href^="#"]').click(function(){
+			if ($($(this).attr('href') ).length ) {
+				var p = $( $(this).attr('href') ).offset();
+				if($(window).width() > 750){
+					$('html,body').animate({ scrollTop: p.top - 170}, 400);
+				}
+				else {
+					$('html,body').animate({ scrollTop: p.top - 100}, 400);
+				}
+			}
+			return false;
+		});
+	});
+});
+
+// anchor top page #
+$(window).bind('load',function(){
+	"use strict";
+ var hash = location.hash;
+ if(hash){
+	  var p1 = $(hash).offset();
+	if($(window).width() > 750){
+					$('html,body').animate({ scrollTop: p1.top - 100}, 400);
+				}
+				else {
+					$('html,body').animate({ scrollTop: p1.top - 100}, 400);
+				}
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
